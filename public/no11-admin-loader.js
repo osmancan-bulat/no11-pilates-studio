6afa6a4354b3af38baceb6a84421ff51b2a06405
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-10 11:39:02 +0300

    feat(admin): load reports module safely

--- public/no11-admin-loader.js
+++ public/no11-admin-loader.js
@@ -6,8 +6,20 @@
     style.dataset.no11Premium = '1';
     document.head.appendChild(style);
   }
+  if (!document.querySelector('link[data-no11-reports]')) {
+    var reportStyle = document.createElement('link');
+    reportStyle.rel = 'stylesheet';
+    reportStyle.href = '/no11-admin-reports.css';
+    reportStyle.dataset.no11Reports = '1';
+    document.head.appendChild(reportStyle);
+  }
   var script = document.createElement('script');
   script.src = '/no11-admin-exact-20.js';
   script.defer = true;
   document.head.appendChild(script);
+
+  var reportScript = document.createElement('script');
+  reportScript.src = '/no11-admin-reports.js';
+  reportScript.defer = true;
+  document.head.appendChild(reportScript);
 })();
