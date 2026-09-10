ba781de8082db97a906b47a6d467fe4f1d8d7923
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-10 17:42:22 +0300

    Refresh admin navigation stabilizer

--- public/no11-admin-loader.js
+++ public/no11-admin-loader.js
@@ -49,7 +49,7 @@
   document.head.appendChild(reportGuard);
 
   var navSafe = document.createElement('script');
-  navSafe.src = '/no11-admin-nav-safe.js?v=20260910-1';
+  navSafe.src = '/no11-admin-nav-safe.js?v=20260910-2';
   navSafe.defer = true;
   document.head.appendChild(navSafe);
 
