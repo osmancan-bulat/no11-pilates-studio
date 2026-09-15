5e047d2f41cfc0673415680e05c9f7db6797233b
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-15 15:18:56 +0300

    Refresh exclusive desktop theme authority

--- public/no11-admin-loader.js
+++ public/no11-admin-loader.js
@@ -61,7 +61,7 @@
   }
 
   var themeAuthority = document.createElement('script');
-  themeAuthority.src = '/no11-theme-authority.js?v=20260915-5';
+  themeAuthority.src = '/no11-theme-authority.js?v=20260915-6';
   themeAuthority.defer = true;
   document.head.appendChild(themeAuthority);
 
