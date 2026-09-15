19f3d6e98b9bfed1181620c194f1c695d057347d
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-15 11:40:08 +0300

    Open desktop appointment date picker

--- public/no11-admin-loader.js
+++ public/no11-admin-loader.js
@@ -48,7 +48,7 @@
   }
 
   var script = document.createElement('script');
-  script.src = '/no11-admin-exact-20.js?v=20260915-2';
+  script.src = '/no11-admin-exact-20.js?v=20260915-3';
   script.defer = true;
   document.head.appendChild(script);
 
