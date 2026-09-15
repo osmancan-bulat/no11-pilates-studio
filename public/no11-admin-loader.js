c007ee9ab3642cdc41ed4762d2dda00e517e9ad0
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-15 12:05:22 +0300

    Enforce single admin renderer instance

--- public/no11-admin-loader.js
+++ public/no11-admin-loader.js
@@ -48,7 +48,7 @@
   }
 
   var script = document.createElement('script');
-  script.src = '/no11-admin-exact-20.js?v=20260915-4';
+  script.src = '/no11-admin-exact-20.js?v=20260915-5';
   script.defer = true;
   document.head.appendChild(script);
 
