f8f891bbbc24f8ce596b2328cd51f4f12956b497
osmancan bulat <osmancanbulat197@gmail.com>
2026-09-19 18:13:36 +0300

    Load final public team and gallery styles

--- app/layout.js
+++ app/layout.js
@@ -6,5 +6,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="tr"><body>{children}</body></html>;
+  return <html lang="tr"><head><link rel="stylesheet" href="/no11-team-live.css?v=4"/><link rel="stylesheet" href="/no11-studio-gallery.css?v=1"/></head><body>{children}</body></html>;
 }
